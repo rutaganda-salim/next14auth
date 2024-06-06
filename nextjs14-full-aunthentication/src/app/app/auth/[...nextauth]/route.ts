f55a581fe5ed5f5fae21b151903c3b46a16e6cec
rutaganda-salim <rutagandasalim@gmail.com>
2024-06-06 20:36:51 +0200

    appbar

---
+++ nextjs14-full-aunthentication/src/app/app/auth/[...nextauth]/route.ts
@@ -0,0 +1,50 @@
+import { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/app/lib/prisma";
+import * as bcrypt from "bcrypt";
+import NextAuth from "next-auth/next";
+
+export const authOptions: AuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                username: {
+                    label: "User Name",
+                    type: "text",
+                    placeholder: "Your user Name",
+                },
+                password: {
+                    label: "Password",
+                    type: "password",
+                    placeholder: "Your Password"
+                },
+            },
+            async authorize(credentials) {
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials?.username,
+                    },
+                });
+
+                if (!user) throw new Error("Username or password is not correct");
+
+                if (!credentials?.password) throw new Error("Please provide your password");
+                
+                const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
+
+                if (!isPasswordCorrect) throw new Error("Username or password is not correct");
+
+                // If the user is found and password is correct, return the user object
+                const {password, ...userWithoutPass}  = user;
+                return userWithoutPass;
+            },
+        }),
+    ],
+};
+
+
+const handler = NextAuth(authOptions);
+
+export {handler as GET , handler as POST};
+
