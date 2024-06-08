f38a7edbee3429e4062ed880c596ec181a4ee758
rutaganda-salim <rutagandasalim@gmail.com>
2024-06-08 17:57:52 +0200

    Password strength checker

--- nextjs14-full-aunthentication/src/components/SignupForm.tsx
+++ nextjs14-full-aunthentication/src/components/SignupForm.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { Checkbox, Input, Link, Button } from "@nextui-org/react";
 import { EnvelopeIcon, EyeIcon, EyeSlashIcon, KeyIcon, PhoneIcon, UserIcon } from "@heroicons/react/20/solid";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { z } from "zod";
 import { Controller, SubmitHandler, useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod";
 import validator, { isMobilePhone } from "validator";
+import { passwordStrength } from "check-password-strength";
+import PasswordStrength from "./PasswordStrength";
 
 const FormSchema = z.object(
     {
@@ -46,7 +48,7 @@ const FormSchema = z.object(
     }
 ).refine(data => data.password === data.ConfirmPassword, {
     message: "Password and Confirm password doesn't match",
-    path: ["Password", "ConfirmPassword"],
+    path: ["ConfirmPassword"],
 }
 
 )
@@ -54,10 +56,25 @@ const FormSchema = z.object(
 type InputType = z.infer<typeof FormSchema>
 
 const SignupForm = () => {
-    const { register, handleSubmit, reset, control, formState: { errors } } = useForm<InputType>({
-        resolver: zodResolver(FormSchema),
-    });
+    const {
+        register,
+        handleSubmit,
+        reset,
+        control,
+        watch,
+        formState: { errors } } = useForm<InputType>({
+            resolver: zodResolver(FormSchema),
+        });
+    const [passStrength, setPassStrength] = useState(0)
     const [isVisiblePass, setIsVisiblePass] = useState(false);
+
+    useEffect(()=>{
+
+        setPassStrength(passwordStrength(watch().password).id)
+
+     }, [watch().password])
+
+
     const toggleVisiblePass = () => setIsVisiblePass((prev) => !prev);
     const saveUser: SubmitHandler<InputType> = async (data) => {
         console.log({ data })
@@ -113,6 +130,7 @@ const SignupForm = () => {
                     )
                 }
             />
+            <PasswordStrength passStrength={passStrength} />
 
             <Input
                 errorMessage={errors.ConfirmPassword?.message}
@@ -126,7 +144,7 @@ const SignupForm = () => {
             <Controller control={control} name="accepted" render={({ field }) => (
 
                 <Checkbox
-                
+
                     onChange={field.onChange}
                     onBlur={field.onBlur}
                     className="col-span-2">
@@ -134,7 +152,7 @@ const SignupForm = () => {
                 </Checkbox>
             )} />
 
-          {!!errors.accepted && <p className="text-red-500">{errors.accepted.message}</p>}
+            {!!errors.accepted && <p className="text-red-500">{errors.accepted.message}</p>}
 
             <div className="flex justify-center col-span-2">
                 <Button type="submit" color="primary" className="w-48">Submit</Button>
