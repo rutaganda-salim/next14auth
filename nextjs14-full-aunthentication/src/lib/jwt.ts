8795d9d42e14c64ddc331fb42f68c47be38082d3
rutaganda-salim <rutagandasalim@gmail.com>
2024-06-09 22:15:34 +0200

    encrypting url in emails using jwt

--- nextjs14-full-aunthentication/src/lib/jwt.ts
+++ nextjs14-full-aunthentication/src/lib/jwt.ts
@@ -1,13 +1,32 @@
-import { JwtPayload } from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
 
 interface SignOption {
-    expiresIn:string | number;
+    expiresIn: string | number;
 }
 
-const DEFAULT_SIGN_OPTION: SignOption ={
-    expiresIn:"1d"
+const DEFAULT_SIGN_OPTION: SignOption = {
+    expiresIn: "1d"
 }
 
-export function signJwt( payload:  JwtPayload, option = DEFAULT_SIGN_OPTION ) {
-    const secretKey = process.env.JWT_USER_ID_SECRET;
+export function signJwt(
+    payload: JwtPayload, option = DEFAULT_SIGN_OPTION
+) {
+
+    const secretKey = process.env.JWT_USER_ID_SECRET!;
+    const token = jwt.sign(payload, secretKey);
+    return token;
+}
+export function verifyJwt(token: string) {
+    try {
+        const secretKey = process.env.JWT_USER_ID_SECRET!;
+        const decoded = jwt.verify(token, secretKey);
+        return decoded as JwtPayload;
+    }
+    catch(e) {
+        console.log(e);
+        return null;
+    }
+
+
 }
